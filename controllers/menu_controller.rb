b509069e3c77c620581c30e9d1f0594130d2e733
Ben <benartw86@yahoo.com>
2017-03-25 19:40:29 +0000

    View Entry Number added to main_menu, assignment complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -15,8 +15,9 @@ class MenuController        #needs methods to display main menu and process user
     puts "1 - View all entries"
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "4 - View entry number"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
     
     selection = gets.to_i       #retrieve user input from the command line using gets
@@ -34,11 +35,15 @@ class MenuController        #needs methods to display main menu and process user
         system "clear"
         search_entries
         main_menu
-      when 4 
+      when 4
+        system "clear"
+        search_by_number
+        main_menu
+      when 5 
         system "clear"
         read_csv
         main_menu
-      when 5
+      when 6
         puts "Goodbye"
         
         exit(0)  #terminates the program, 0 signals exit without errors
@@ -82,6 +87,21 @@ class MenuController        #needs methods to display main menu and process user
   end
   
   def search_entries
+  end
+  
+  def search_by_number
+    print "Which entry number would you like to view? "
+    selection = gets.chomp.to_i
+      if selection < address_book.entries.count
+        puts "You selected #{selection}"
+        puts address_book.entries[selection]
+        puts "To exit type any letter!"
+        gets.chomp
+        system "clear"
+      else  
+        puts "Please enter a valid number!"
+        search_by_number
+      end
   end
   
   def read_csv
